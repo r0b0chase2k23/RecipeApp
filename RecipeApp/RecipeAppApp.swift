00b84fffca8ab86a12ed217d6ceadd7bc9806034
Robert Chase <r0b0chase2k18@gmail.com>
2022-05-08 23:59:38 +0100

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Robert Chase on 08/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
